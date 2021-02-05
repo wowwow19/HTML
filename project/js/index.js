933d21a69387af4fa97e7dccde9942f274e46272
wowwow19 <wowwow19@naver.com>
2021-02-05 23:26:17 +0900

    210205 회원가입폼 구현

--- project/js/index.js
+++ project/js/index.js
@@ -1,3 +1,4 @@
+// 실시간 검색어
 $(function () {
   var ticker = function () {
     setTimeout(function () {
@@ -10,10 +11,32 @@ $(function () {
   ticker();
 });
 
+// 즐겨찾기 토글
+$(function () {
+  $("span").click(function () {
+    $("#fav-btn", this).toggleClass("far fas");
+  }); 
+});
+
+// 좋아요 토글
+$(function () {
+  $("span").click(function () {
+    $("#like-btn", this).toggleClass("like-btn like-btn-clicked");
+  });
+});
+
+$(function () {
+  $("span").click(function () {
+    $("#like-btn", this).toggleClass("far fas");
+  });
+});
+
+// 오버시 이미지 교체 함수
 function changeImg(img) {
   var mapimg = document.getElementById('map');
   mapimg.src = img;
 }
+
 function changeImg1(obj, img) {
   var mapimg1 = document.getElementById('map1');
   mapimg1.src = img;
